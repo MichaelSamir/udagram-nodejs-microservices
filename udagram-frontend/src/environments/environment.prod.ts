c17a6448bfdb296bcef119fd3d88b93893439306
Michael <michael.samir.jacob@gmail.com>
2023-02-13 16:55:16 +0200

    change apiHost

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://ac3cfb30093774f6f9180570301dbdb8-1659946479.us-east-1.elb.amazonaws.com:8080/api/v0/'
+  apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
