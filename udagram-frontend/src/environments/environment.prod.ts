d82060e0c80e7db6aff3e025a3ca9004476cf892
Michael <michael.samir.jacob@gmail.com>
2023-02-17 04:01:30 +0200

    edit apiHost

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aa01dc50943ca415ca2bd3d604b3a93f-1516618237.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
